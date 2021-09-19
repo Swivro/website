11eb3f694760d20333b76f3c0fc6594a542283a4
Kasper Ireland <54741819+kasperireland@users.noreply.github.com>
2021-09-18 20:49:18 -0400

    Update sync.bat

--- sync.bat
+++ sync.bat
@@ -1,3 +1,4 @@
+# This script is used to sync website changes with the git repository, for the web server to then sync with the git repository, to make all site changes live. WINDOWS ONLY.
 git add *
 git commit -m "Website modifications"
 git push -u origin main

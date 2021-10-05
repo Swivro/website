cbd5ea4ffcbfbeb42b2fb025fb41c6e3ab4350b8
Albert Arnaud The Best <matthavoc101@gmail.com>
2021-10-04 21:57:14 -0400

    add new comment line to sync.bat

--- sync.bat
+++ sync.bat
@@ -1,4 +1,5 @@
 # This script is used to sync website changes with the git repository, for the web server to then sync with the git repository, to make all site changes live. WINDOWS ONLY.
+# As of october 4th, 2021, this script isn't really used much anymore, but I'll probably use it in the near future, so I am keeping it here :)
 git add *
 git commit -m "Website modifications"
 git push -u origin main
